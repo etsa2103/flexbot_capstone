f63fa0bfdac9752b9dc7bae10d3167e7513dbdba
etsa <etsa@seas.upenn.edu>
2026-03-30 19:03:45 -0400

    cleaned up robot init

--- init_bot.sh
+++ init_bot.sh
@@ -1,5 +1,5 @@
-python3 ~/kr_flexbot/MOTORS/init_left_motor.py
-python3 ~/kr_flexbot/MOTORS/init_right_motor.py
-g++ -std=c++17 -Wall -Wextra ~/kr_flexbot/UDP/imu_udp_tx.cpp -o main
-g++ -std=c++17 -Wall -Wextra -pthread ~/kr_flexbot/UDP/motor_controller.cpp -o main
-python3 /Home 
+python3 ~/flexbot_capstone/MOTORS/init_left_motor.py
+python3 ~/flexbot_capstone/MOTORS/init_right_motor.py
+g++ -std=c++17 -Wall -Wextra ~/flexbot_capstone/UDP/imu_udp_tx.cpp -o main
+g++ -std=c++17 -Wall -Wextra -pthread ~/flexbot_capstone/UDP/motor_controller.cpp -o main
+python3 /root/flexbot_capstone/UDP/drive_i7.py
