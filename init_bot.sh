244aeac1750fb1fa360d108375ee135105ea3f80
wheatthin <essanchez02@gmail.com>
2026-03-31 17:24:40 -0400

    Updated init_bot script to run the right bash scripts

--- init_bot.sh
+++ init_bot.sh
@@ -1,5 +1,7 @@
-python3 ~/flexbot_capstone/MOTORS/init_left_motor.py
-python3 ~/flexbot_capstone/MOTORS/init_right_motor.py
-g++ -std=c++17 -Wall -Wextra ~/flexbot_capstone/UDP/imu_udp_tx.cpp -o main
-g++ -std=c++17 -Wall -Wextra -pthread ~/flexbot_capstone/UDP/motor_controller.cpp -o main
-python3 /root/flexbot_capstone/UDP/drive_i7.py
+#!/bin/bash
+#python3 ~/flexbot_capstone/MOTORS/init_left_motor.py
+#python3 ~/flexbot_capstone/MOTORS/init_right_motor.py
+#g++ -std=c++17 -Wall -Wextra ~/flexbot_capstone/UDP/imu_udp_tx.cpp -o main
+#g++ -std=c++17 -Wall -Wextra -pthread ~/flexbot_capstone/UDP/motor_controller.cpp -o main
+bash ~/flexbot_capstone/UDP/init_motors.sh
+bash ~/flexbot_capstone/UDP/start_comm.sh
