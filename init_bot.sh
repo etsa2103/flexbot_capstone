e7d6a098281cf48e4c38504c624b19f715373b80
etsa <etsa@seas.upenn.edu>
2026-04-16 20:12:13 +0000

    edited init_bot script to have a delay

--- init_bot.sh
+++ init_bot.sh
@@ -3,6 +3,7 @@
 #python3 ~/flexbot_capstone/MOTORS/init_right_motor.py
 #g++ -std=c++17 -Wall -Wextra ~/flexbot_capstone/UDP/imu_udp_tx.cpp -o main
 #g++ -std=c++17 -Wall -Wextra -pthread ~/flexbot_capstone/UDP/motor_controller.cpp -o main
+sleep 3
 bash ~/flexbot_capstone/MOTORS/init_motors.sh
 bash ~/flexbot_capstone/BAT/init_bat.sh
 bash ~/flexbot_capstone/UDP/start_comm.sh
